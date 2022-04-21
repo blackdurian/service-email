9a3e05309e0989361e2df537a763984f88f2b75c
Wei Quan <weiquan.lo@simplr.com.my>
2022-04-21 13:55:50 +0800

    project inital

---
+++ src/test/java/com/myproject/serviceemail/ServiceEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.serviceemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
